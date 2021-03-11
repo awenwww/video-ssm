3f94cd4c6c5e236fccee32e8f1cff9b0ba19e224
a_wen <2223003379@qq.com>
2021-03-11 11:20:39 +0800

    测试

--- src/main/java/com/qf/util/MailUtils.java
+++ src/main/java/com/qf/util/MailUtils.java
@@ -84,7 +84,6 @@ public final class MailUtils {
     public static void main(String[] args) throws Exception { // 做测试用
         MailUtils.sendMail("3441565606@qq.com", "你好，这是一封测试邮件，无需回复。", "测试邮件随机生成的验证码是：" + getValidateCode(6));
         System.out.println("发送成功");
-        System.out.println("1111");
 
     }
 }
